573256dc86e2328e7682a69dfda682a718b1f8d0
borislavkokeza <borislav.kokeza@planetsoft.eu>
2025-09-24 18:47:14 +0200

    Implement TDD for categories redux slice. Install tanstack query

--- web/src/features/categories/categoriesSlice.test.ts
+++ web/src/features/categories/categoriesSlice.test.ts
@@ -1,10 +1,47 @@
-import reducer from "./categoriesSlice";
+import reducer, {
+    CategoriesSliceState,
+    hideAddCategory,
+    hideUpdateCategory,
+    showAddCategory,
+    showUpdateCategory
+} from "./categoriesSlice";
+
+const baseState: CategoriesSliceState = {
+    isAddCategoryOpen: false,
+    categoryToUpdate: null,
+}
+
+function extendBaseState(override: Partial<CategoriesSliceState>): CategoriesSliceState {
+    return { ...baseState, ...override };
+}
 
 describe('categories reducer', () => {
     it('should handle initial state', () => {
         const initialState = reducer(undefined, { type: 'unknown' });
-        expect(initialState).toEqual({
-            isAddCategoryOpen: false,
-        });
+        expect(initialState).toEqual(baseState);
     });
+
+    it('should handle showAddCategory', () => {
+        const initialState = extendBaseState({ isAddCategoryOpen: false });
+        const nextState = reducer(initialState, showAddCategory());
+        expect(nextState).toEqual(extendBaseState({ isAddCategoryOpen: true }));
+    });
+
+    it('should handle hideAddCategory', () => {
+        const initialState = extendBaseState({ isAddCategoryOpen: true });
+        const nextState = reducer(initialState, hideAddCategory());
+        expect(nextState).toEqual(extendBaseState({ isAddCategoryOpen: false }));
+    });
+
+    it('should handle showUpdateCategory', () => {
+        const initialState = extendBaseState({ categoryToUpdate: null });
+        const nextState = reducer(initialState, showUpdateCategory({ id: '1', name: 'Category 1' }));
+        expect(nextState).toEqual(extendBaseState({ categoryToUpdate: { id: '1', name: 'Category 1' } }));
+    });
+
+    it('should handle hideUpdateCategory', () => {
+        const initialState = extendBaseState({ categoryToUpdate: { id: '1', name: 'Category 1' } });
+        const nextState = reducer(initialState, hideUpdateCategory());
+        expect(nextState).toEqual(extendBaseState({ categoryToUpdate: null }));
+    })
 });
--- web/src/features/categories/categoriesSlice.ts
+++ web/src/features/categories/categoriesSlice.ts
@@ -1,18 +1,39 @@
 import {createSlice} from "@reduxjs/toolkit";
 
+type Category = {
+    id: string;
+    name: string;
+}
+
 export type CategoriesSliceState = {
     isAddCategoryOpen: boolean;
+    categoryToUpdate: Category | null
 }
 
 const initialState: CategoriesSliceState = {
     isAddCategoryOpen: false,
+    categoryToUpdate: null,
 };
 
 const categoriesSlice = createSlice({
     name: 'categories',
     initialState,
     reducers: {
+        showAddCategory(state) {
+            state.isAddCategoryOpen = true;
+        },
+        hideAddCategory(state) {
+            state.isAddCategoryOpen = false;
+        },
+        showUpdateCategory(state, action) {
+            state.categoryToUpdate = action.payload;
+        },
+        hideUpdateCategory(state) {
+            state.categoryToUpdate = null;
+        }
     },
 });
+
+export const {showAddCategory, hideAddCategory, showUpdateCategory, hideUpdateCategory} = categoriesSlice.actions;
 
 export default categoriesSlice.reducer;
